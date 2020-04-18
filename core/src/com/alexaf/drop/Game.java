d4d2d30f539bf52ea101f6d0d5a65c0d88bb271f
Alex A.F <alex2andres@gmail.com>
2020-04-18 12:31:30 +0200

    Add rain & drop images and sounds

--- core/src/com/alexaf/drop/Game.java
+++ core/src/com/alexaf/drop/Game.java
@@ -2,18 +2,25 @@ package com.alexaf.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture dropImage, bucketImage;
+	Sound dropSound;
+	Music rainMusic;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		dropImage = new Texture("drop.png");
+		bucketImage = new Texture("bucket.png");
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 	}
 
 	@Override
@@ -21,13 +28,13 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(dropImage, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		dropImage.dispose();
 	}
 }
